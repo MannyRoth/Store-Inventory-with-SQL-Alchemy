70228c6f305112a0fae9bfc48e71e46e0b90d03e
MannyRoth <98435897+MannyRoth@users.noreply.github.com>
2022-03-08 14:40:40 -0500

    Add files via upload

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
-
-from itertools import product
+from sqlalchemy import Integer
 from models import Base, session, Inventory, engine
+import time
 import datetime
 import csv
 
@@ -16,18 +16,28 @@ def menu():
 
 
 def clean_price(price_str):
-    split_price = price_str.split('$')
-    price_float = float(split_price[1])
-    return int(price_float * 100)
-    
+    try:
+        split_price = price_str.split('$') 
+        price_float = float(split_price[1])
+        return_price = int(price_float * 100)
+    except IndexError:
+        print('Please input correctly formmatted Price')
+        return
+    else:
+        return return_price
 
 def clean_date(date_str):
     split_date = date_str.split('/')
-    print(split_date)
-    month = int(split_date[0])
-    day = int(split_date[1])
-    year = int(split_date[2])
-    return datetime.date(year, month, day)
+    try:
+        month = int(split_date[0])
+        day = int(split_date[1])
+        year = int(split_date[2])
+        return_date = datetime.date(year, month, day)
+    except ValueError:
+        print('Please input correctly formmatted Date')
+        return
+    else:
+        return return_date
 
 
 def add_csv():
@@ -44,23 +54,40 @@ def add_csv():
                 new_product = Inventory(product_name = product_name, product_price = product_price, product_quantity = product_quantity, date_updated = date_updated)
                 session.add(new_product)
         session.commit()
+        
             
-
-
-
-
 
 def app():
     app_running = True
     while app_running:
         choice = menu()
         if choice == 'v':
-            pass
+            for item in session.query(Inventory):
+                print(f'{item.product_id} | {item.product_name} | {item.product_quantity} | {item.product_price} | {item.date_updated}')
+            input('Press Enter to return to Main Menu')
+            
         elif choice == 'a':
-            pass
+            product_name = input('Product Name: ')
+            product_quantity = input('Product Quantity: ')
+            
+            while True:
+                product_price = input('Price (ex:$12.99): ')
+                product_price_int = clean_price(product_price)
+                if product_price_int != None:
+                    break
+            while True:
+                date_updated = input('Current Date (ex:1/22/3333): ')
+                date_updated = clean_date(date_updated)
+                if type(date_updated) == datetime.date:
+                    break
+            new_product = Inventory(product_name = product_name, product_price = product_price, product_quantity = product_quantity, date_updated = date_updated)
+            session.add(new_product)
+            session.commit()
+            print('Product Added!')
+            time.sleep(1.5)
         elif choice == 'b':
             pass
-        else:
+        elif choice == 'c':
             print('Goodbye')
             app_running = False
 
@@ -71,8 +98,10 @@ def app():
 
 if __name__ == '__main__':
     Base.metadata.create_all(engine)
-    #app()
     add_csv()
+    app()
     
+
     for item in session.query(Inventory):
         print(item)
+
